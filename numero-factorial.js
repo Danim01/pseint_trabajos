cae97ead784f3db50b63c6044498fa6c14694207
Danim01 <146406663+Danim01@users.noreply.github.com>
2024-02-20 17:18:25 -0500

    Create numero-factorial.js

---
+++ numero-factorial.js
@@ -0,0 +1,21 @@
+let numero="4";
+let prueba=isNaN("perro");
+
+function calcularFactorial(numero){
+  if (isNaN(numero)){
+  	console.log("no es un numero");
+  } else if (numero<0){
+    	console.log("ingrese un numero entero positivo")
+    } else if (numero===1 || numero===0){
+      	factorial=1;
+      } else {
+        	let factorial=1;
+            for (i=1;i <= numero;i++){
+              factorial = factorial * i;
+      }
+      }
+        return factorial;
+      }
+calcularFactorial(numero);
+const resultadoFactorial = calcularFactorial(numero);
+console.log(resultadoFactorial);
